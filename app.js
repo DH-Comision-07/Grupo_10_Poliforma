59d3f9d15dd87897a30c5ddecbd11c72860159f0
rubenferrerl <rubenferrerl@gmail.com>
2024-02-27 01:19:08 -0400

    feature/conexion entre login y register

--- app.js
+++ app.js
@@ -13,4 +13,9 @@ app.get('/register.html', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 })
 
+app.get('/login.html', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
+
 app.listen(3030,() => console.log('servidor corriendo en el puerto 3030'));
