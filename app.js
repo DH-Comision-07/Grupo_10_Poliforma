11c1d19147de2d2a8cbb576fe2e54b68d8557d98
rubenferrerl <rubenferrerl@gmail.com>
2024-03-01 00:03:51 -0400

    fix/app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/index.html'))
 })
 
-app.get('/productCart', (req, res) =>{
+app.get('/productCart.html', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/productCart.html'))
 })
 
