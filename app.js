72829e27670c42facca7ae8554d23bc4e7ea6872
rubenferrerl <rubenferrerl@gmail.com>
2024-02-29 21:50:23 -0400

    feature/se hicieron las conexiones

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) );
 
-app.get('/', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './views/index.html'))
+app.get('/productCart', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
 })
 
 app.listen(3030,() => console.log('servidor corriendo en el puerto 3030'));
