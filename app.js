94f2dde9ab0089de65d29ef35fc13f40aff78d72
rubenferrerl <rubenferrerl@gmail.com>
2024-02-29 23:59:06 -0400

    fix/app.js

--- app.js
+++ app.js
@@ -6,19 +6,15 @@ const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) );
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './views/index.html'))
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
 
-<<<<<<<<< Temporary merge branch 1
-app.get('/register.html', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './views/register.html'))
-=========
+app.get('/productCart', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+})
+
 app.get('/productDetail.html', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
-})
-
-app.get('/register.html', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './views/register.html'))
 })
 
 app.get('/login.html', (req, res) =>{
@@ -28,11 +24,5 @@ app.get('/login.html', (req, res) =>{
 app.get('/register.html', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 })
-
-
-app.get('/register.html', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './views/register.html'))
-})
->>>>>>>>> Temporary merge branch 2
 
 app.listen(3030,() => console.log('servidor corriendo en el puerto 3030'));
