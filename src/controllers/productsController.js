a2a217b0d1f51ffcb01a13f0d93dae124139a708
rubenferrerl <rubenferrerl@gmail.com>
2024-03-13 14:02:38 -0400

    feature/conexion del createProduct

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,7 +1,8 @@
 let products = {
     home: (req,res)=>res.send("todos los productos"),
     getDetail: (req,res)=> res.render("products/productDetail"),
-    getCart: (req,res)=> res.render("products/productCart")
+    getCart: (req,res)=> res.render("products/productCart"),
+    getCreate: (req, res)=> res.render("products/createProduct")
 }
 
 module.exports = products;
