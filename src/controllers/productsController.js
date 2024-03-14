de602869c8ba6ad982bbae457c30590a529aefe9
imanol003 <imanolaparicio24@gmail.com>
2024-03-14 16:10:31 -0300

    primer editar productos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,7 +2,8 @@ let products = {
     home: (req,res)=>res.send("todos los productos"),
     getDetail: (req,res)=> res.render("products/productDetail"),
     getCart: (req,res)=> res.render("products/productCart"),
-    getCreate: (req, res)=> res.render("products/createProduct")
+    getCreate: (req, res)=> res.render("products/createProduct"),
+    getEdit: (req, res)=> res.render("products/editProduct")
 }
 
 module.exports = products;
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -5,6 +5,7 @@ const  productsController = require("../controllers/productsController");
 router.get("/",(req,res)=>res.send("todos los productos"));
 router.get("/productDetail",productsController.getDetail);
 router.get("/productCart",productsController.getCart);
-router.get("/createProduct", productsController.getCreate)
+router.get("/createProduct", productsController.getCreate);
+router.get("/editProduct", productsController.getEdit)
 
 module.exports = router;
