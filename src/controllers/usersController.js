b79427eee4c9c1dca19f0875bfd02f36381ca0be
rubenferrerl <rubenferrerl@gmail.com>
2024-04-26 20:51:52 -0400

    feature/se hizo el controller de usuarios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,24 @@
 let users = {
-    login: (req,res)=> res.render("users/login"),
-    register: (req,res)=> res.render("users/register")
+    login: function(req,res){ 
+        res.render("users/login")
+    },
+    register: function(req,res){ 
+        res.render("users/register")
+    },
+    userProfile: function(){
+
+    },
+    editUser: function(){
+
+    },
+    deleteUser: function(){
+
+    },
+    dashboardUsers: function(){
+
+    },
+
+
 }
 
 module.exports = users;
