84472d14215f88a58ba176ffbc3635391da743ad
imanol003 <imanolaparicio24@gmail.com>
2024-04-29 15:13:54 -0300

    authMid

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const  productsController = require("../controllers/productsController");
 const uploadFile = require('../middlewares/multerMid');
+const authMid = require('../middlewares/authMid');
 
 router.get("/",productsController.home);
 router.get("/productDetail/:id",productsController.detail);
-router.get('/dashboard', productsController.dashboard);
+router.get('/dashboard', authMid, productsController.dashboard);
 router.get("/productCart",productsController.cart);
-router.get("/dashboard", productsController.dashboard);
+router.get("/dashboard", authMid, productsController.dashboard);
 
-router.get("/createProduct", productsController.create);
+router.get("/createProduct", authMid, productsController.create);
 router.post("/", uploadFile.single("imagenProducto"), productsController.store);
 
 router.get("/editProduct/:id", productsController.edit);
