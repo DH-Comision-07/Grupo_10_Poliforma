b6f25423f00612d4ef576156c679dc20d289bcca
rubenferrerl <rubenferrerl@gmail.com>
2024-05-27 10:33:20 -0400

    feature/ se hizo el modelo de producto

---
+++ src/model/db/models/Producto.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes)=>{
+
+    let alias = 'Productos';
+    let cols ={
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primeryKey: true
+        },
+        nombre:{
+            type: DataTypes.STRING(45),
+            allowNull: false
+        },
+        descripcion:{
+            type: DataTypes.TEXT('long'),
+            allowNull: false
+        },
+        imagen:{
+            type: DataTypes.STRING(45),
+            allowNull: false
+        },
+        precio:{
+            type: DataTypes.DECIMAL(10,2),
+            allowNull: false
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        descuento: DataTypes.STRING(45)
+    };
+    let config  = {
+        tableName: 'productos',
+        timeStamps: false
+    };
+
+    let Producto = sequelize.define(alias, cols, config);
+
+    return Producto;
+}
