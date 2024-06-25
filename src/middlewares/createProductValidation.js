3091dbb295e04f54d68e28d96ff44bb83a68a7be
Ruben Ferrer <rubenferrerl@gmail.com>
2024-06-25 15:14:47 -0400

    fix/formato imagenes

--- src/middlewares/createProductValidation.js
+++ src/middlewares/createProductValidation.js
@@ -16,7 +16,7 @@ check("precio").notEmpty().withMessage("No puede estar vacio"),
     
 check("imagenProducto").custom((value, { req }) => {
     let file = req.file;
-    let acceptedExtensions = [".jpg",".jpeg", ".png",".webp"]; 
+    let acceptedExtensions = [".jpg",".jpeg", ".PNG",".webp"]; 
     
     if (!file) {
         throw new Error("Tiene que subir una imagen");
