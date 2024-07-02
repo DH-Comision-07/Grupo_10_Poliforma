6c825d0c04aa169157cb665778f8d7ce35a77ec4
MiyuKubota <lmasganador@gmail.com>
2024-07-02 10:40:01 -0300

    Actualizacion de lo que seria la edicion de productos, pero faltaria revisarlo en grupo

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -19,7 +19,7 @@ const controller = {
    // edit: function (req, res){
      //   res.render("products/editProduct", {productToEdit: productsService.getOneBy(req.params.id)})
      // Antes que yo Denis creara el editar, osea el Update estaban estas dos lineas de codigo que estan aca arriba, las cuales las dejé como comentarios para no this.eliminarlas.
-     edit: function (req, res) {
+     editPdoructo: function (req, res) {
         let pedidoProductos = db.poliforma_10.findByPK(req.params.id);
 
         let pedidoCategorias = de.poliforma_10-findAll();
@@ -29,15 +29,37 @@ const controller = {
             res.render("editarProductos", {productos:productos, categorias:categorias})
         })
      },
-    actualizar: funtion(req, res) {
+    actualizarProducto: async function (req, res){
         db.poliforma_10.update({
-             // (Copiar y pegar lo que se escribío del create y modificar donde dice create por update)
+        try {
+            let resultValidations = validationResult(req);
+            if(resultValidations.errors.length > 0){
+                res.redirect('/products/:id')
+                //chequiar si el esta bien escrita la linea de codigo 36; el /id me llama la atención
+            }else{
+            let editProduct = {
+                nombre: req.body.NombreProducto,
+                descripcion:req.body.descripcion,
+                tags: req.body.tags,
+                imagen: req.file.filename,
+                categorias_id: req.body.categoria,
+                precio: req.body.precio,
+                stock: req.body.stock,
+                descuento: req.body.descuento
+            }
+            await productsService.save(newProduct);
+            res.redirect("/products/dashboard");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
     }, {
         where: {
             id: req.params.id
         }
     });
-        res.render('users/products', {productsToEdit: produtsService.getOneBy(req.params.id)})
+        res.render('users/products/:id', {productsToEdit: produtsService.getOneBy(req.params.id)})
        // res.redirect(("/productDetail/:id",productsController.detail);)
      }
     },
